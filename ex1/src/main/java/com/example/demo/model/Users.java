493699265f2d52404fc72dbb81b04e014a818270
Big-waterme1on <dntjr8777@naver.com>
2025-02-17 14:43:05 +0900

    모델 유저 슛~

---
+++ ex1/src/main/java/com/example/demo/model/Users.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Users {
+	private String user_id;
+	private String password;
+	private String user_name;
+	private String address;
+	private String address_detail;
+	private Integer number;
+	private String email;
+	private String birth;
+	private String phone;
+	private Integer grade;
+
+}
