956cc9c957ec0a907fe966c8b5851f3d12361f6d
wjddlsdyd01 <jungjungkk@naver.com>
2025-02-17 16:25:34 +0900

    Add Qna.java and QnaAns.java

---
+++ ex1/src/main/java/com/example/demo/model/QnaAns.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QnaAns {
+	private Integer qna_number;
+	private Integer qna_ans;
+	private String ans_title;
+	private String ans_content;
+}
