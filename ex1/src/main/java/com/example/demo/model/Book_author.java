a16ca4c1a46161b47649d2e6278d926a2afc1257
Big-waterme1on <dntjr8777@naver.com>
2025-02-17 15:58:48 +0900

    a

---
+++ ex1/src/main/java/com/example/demo/model/Book_author.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book_author {
+	private Integer isbn;
+	private String author;
+}
---
+++ ex1/src/main/java/com/example/demo/model/Review.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Review {
+	private Integer review_id;
+	private Integer isbn;
+	private String user_id;
+	private Integer rating;
+	private String content;
+	private String reg_date;
+}
