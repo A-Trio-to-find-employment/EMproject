cd28ee8bd6233262a6e1c4483b34c0c9749d9df3
mgclkoh <rhaudcjf8@mjc.ac.kr>
2025-02-20 10:02:23 +0900

    회원정보 완

--- ex1/src/main/java/com/example/demo/model/Users.java
+++ ex1/src/main/java/com/example/demo/model/Users.java
@@ -19,18 +19,21 @@ public class Users {
     private String password;
 
     @NotNull(message = "이름은 필수 입력 항목입니다.")
+    @Size(min = 1, message = "사용할 닉네임을 입력해주세요")
     private String user_name;
 
     @NotNull(message = "주소는 필수 입력 항목입니다.")
     private String address;
 
     @NotNull(message = "상세 주소는 필수 입력 항목입니다.")
+    @Size(min = 1,message = "상세 주소를 입력해야 상품이 제대로 갑니다.")
     private String address_detail;
     
     @NotNull(message = "우편번호는 입력 필수 항목입니다.")
     private String zipcode;
 
     @NotNull(message = "이메일은 필수 입력 항목입니다.")
+    @Size(min = 1, message = "수신 받으실 이메일을 입력해주세요.")
     @Email(message = "유효한 이메일 주소를 입력해주세요.")
     private String email;
 
