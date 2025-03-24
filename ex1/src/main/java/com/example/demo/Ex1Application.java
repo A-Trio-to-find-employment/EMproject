3c2736c6c24b03cbb98bc1c8872f2bda4cba90a1
Big-waterme1on <dntjr8777@naver.com>
2025-03-24 11:36:17 +0900

    회원가입 수정

--- ex1/src/main/java/com/example/demo/Ex1Application.java
+++ ex1/src/main/java/com/example/demo/Ex1Application.java
@@ -2,10 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Ex1Application {
+public class Ex1Application extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(Ex1Application.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(Ex1Application.class);
+	}
+
+	
 }
